d060045ddb9041ab89dcbb34f827d04019e0700e
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2025-09-19 16:17:30 +0545

    Initial Commit  - Folder Setup

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import app from "./app";
+import { CONFIG } from "./config";
+import logger from "./config/logger";
+
+const startServer = () => {
+    try {
+        app.listen(CONFIG.PORT, () => {
+            logger.info(`Server is listening at port: ${CONFIG.PORT}`);
+        });
+    } catch (error) {
+        logger.info(error);
+    }
+};
+const boj = {
+    name: "ramesh",
+};
+const b = boj.name;
+console.log(b);
+startServer();
